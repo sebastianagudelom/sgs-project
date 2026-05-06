743da8887da1b1ea7d65dbd7aab34f7d9fd8dc57
sebastianagudelom <sebastianagudelomendez@gmail.com>
2026-05-06 12:31:41 -0500

    fix: karma config minimal sin override del provider @angular/build

--- sgs-frontend/karma.conf.js
+++ sgs-frontend/karma.conf.js
@@ -1,16 +1,5 @@
 module.exports = function (config) {
   config.set({
-    basePath: '',
-    frameworks: ['jasmine', '@angular/build'],
-    plugins: [
-      require('karma-jasmine'),
-      require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
-      require('karma-coverage'),
-    ],
-    client: { jasmine: { random: false } },
-    coverageReporter: { dir: require('path').join(__dirname, './coverage'), subdir: '.', reporters: [{ type: 'text-summary' }] },
-    reporters: ['progress'],
     browsers: ['ChromeHeadlessCI'],
     customLaunchers: {
       ChromeHeadlessCI: {
@@ -19,5 +8,6 @@ module.exports = function (config) {
       },
     },
     singleRun: true,
+    restartOnFileChange: false,
   });
 };
