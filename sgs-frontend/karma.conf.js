bd90591fead62c61aa1b1069c0b834db338408a1
sebastianagudelom <sebastianagudelomendez@gmail.com>
2026-05-06 12:13:16 -0500

    fix: karma config con ChromeHeadlessCI para Jenkins

---
+++ sgs-frontend/karma.conf.js
@@ -0,0 +1,25 @@
+module.exports = function (config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular/build'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage'),
+      require('@angular/build/plugins/karma'),
+    ],
+    client: { jasmine: { random: false } },
+    jasmineHtmlReporter: { suppressAll: true },
+    coverageReporter: { dir: require('path').join(__dirname, './coverage'), subdir: '.', reporters: [{ type: 'html' }, { type: 'text-summary' }] },
+    reporters: ['progress', 'kjhtml'],
+    browsers: ['ChromeHeadlessCI'],
+    customLaunchers: {
+      ChromeHeadlessCI: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox', '--disable-gpu', '--disable-dev-shm-usage'],
+      },
+    },
+    restartOnFileChange: true,
+  });
+};
