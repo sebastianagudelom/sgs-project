93ed25207d5ce7eba69552676c19a6b4d8edff5e
sebastianagudelom <sebastianagudelomendez@gmail.com>
2026-05-13 20:14:24 -0500

    act

--- sgs-backend/src/main/java/com/uniquindio/backend/SgsBackendApplication.java
+++ sgs-backend/src/main/java/com/uniquindio/backend/SgsBackendApplication.java
@@ -11,12 +11,12 @@ public class SgsBackendApplication {
 	/* Main method to run the Spring Boot application */
 	public static void main(String[] args) {
 		SpringApplication.run(SgsBackendApplication.class, args);
+		System.out.println("SGS Backend Application is running...");
 	}
 
 
 
 
 
-	/*
-	fsefjiosenfies */
+
 }
